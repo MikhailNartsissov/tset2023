4f1828da04434eab84582393188c0a76b19abdd4
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2023-12-23 14:52:47 +0300

    0004-superuser-created-data-models-registered-in-admin

--- human_friends/homeanimals/admin.py
+++ human_friends/homeanimals/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    AnimalClass,
+    Animal,
+    Commands,
+)
 
-# Register your models here.
+admin.site.register(AnimalClass)
+admin.site.register(Commands)
+admin.site.register(Animal)
